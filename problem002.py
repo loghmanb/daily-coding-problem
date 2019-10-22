4ef5f27f0efd65a19f13e4432c6c61a6a5fa8a1c
Loghman Barari <loghmanb@gmail.com>
2019-10-22 18:59:25 +0330

    solve multiple before/after elements in array without using division

--- problem002.py
+++ problem002.py
@@ -18,9 +18,39 @@ def solve(int_list):
     return list(map(lambda x:int(all_product/x), int_list))
 
 
+'''
+for [a, b, c, d, e]
+
+first step:
+    for each item is calculated from previous value to previous position value in main array
+    l1 = [1. a, (a)b, (ab)c, (abc)d]
+second step;
+    repeat what happend before in reverse (from right to left)
+    l2 = [b(cde), c(de), d(e), e, 1]
+and finally:
+    ans = l1[i]*l2[i] for each item
+'''
+def solve_without_division(int_list):
+    n = len(int_list)
+
+    #from left t right
+    l1 = [1]*n
+    for i in range(1, n):
+        l1[i] = l1[i-1] * int_list[i-1]
+
+    #from right to left
+    l2 = [1]*n
+    for i in range(1, n):
+        l2[n-i-1] = l2[n-i] * int_list[n-i]
+
+    ans = [l1[i]*l2[i] for i in range(n)]
+
+    return ans
+
+
 if __name__ == '__main__':
     test_list = [[1, 2, 3, 4, 5],
                  [3, 2, 1]]
     
     for l in test_list:
-        print( 'list: ', l, ' output: ', solve(l) )
+        print( 'list: ', l, ' output#1: ', solve(l), 'output#2: ', solve_without_division(l) )
