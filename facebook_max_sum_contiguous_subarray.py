bc32f4ef7d5abdddaa3d86de8992fe1c3af1eb19
loghmanb <loghmanb@gmail.com>
2019-10-27 17:49:41 +0330

    solve by O(N)

---
+++ facebook_max_sum_contiguous_subarray.py
@@ -0,0 +1,65 @@
+'''
+Max Sum Contiguous Subarray
+https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
+
+Asked in: Facebook, Paypal, Yahoo, Microsoft, LinkedIn, Amazon, Goldman Sachs
+
+Find the contiguous subarray within an array, A of length N which has the largest sum.
+
+Input Format:
+
+The first and the only argument contains an integer array, A.
+Output Format:
+
+Return an integer representing the maximum possible sum of the contiguous subarray.
+Constraints:
+
+1 <= N <= 1e6
+-1000 <= A[i] <= 1000
+For example:
+
+Input 1:
+    A = [1, 2, 3, 4, -10]
+
+Output 1:
+    10
+
+Explanation 1:
+    The subarray [1, 2, 3, 4] has the maximum possible sum of 10.
+
+Input 2:
+    A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+Output 2:
+    6
+
+Explanation 2:
+    The subarray [4,-1,2,1] has the maximum possible sum of 6.
+'''
+
+
+def maxSubArray(self, A):
+    if not A: return
+    
+    max_subA = A[0]
+    subA = 0
+    for i,x in enumerate(A):
+        if i==0: 
+            subA = x
+        elif subA>0 and (x+subA)>0:
+            subA += x
+        else:
+            subA = x
+        if max_subA<subA:
+            max_subA = subA
+    return max_subA
+
+
+if __name__=='__main__':
+    d = [
+        [[1, 2, 3, 4, -10], 10],
+        [[-2, 1, -3, 4, -1, 2, 1, -5, 4], 6]
+    ]
+
+    for d in data:
+        print('input', d[0], 'output', maxSubArray(d[0]))
