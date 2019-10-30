c6c3d9e07a306ea1922dd94c57cb007bab5c3d34
Loghman Barari <loghmanb@gmail.com>
2019-10-30 15:41:09 +0330

    revise solution by interviewbit hints

--- inmobi_first_missing_integer.py
+++ inmobi_first_missing_integer.py
@@ -1,6 +1,11 @@
-  
 '''
-This problem was asked by Stripe.
+First Missing Integer
+
+Asked by Model N, InMobi, Amazon, Stripe
+
+https://www.interviewbit.com/problems/first-missing-integer/
+
+This problem was asked by . 
 Given an array of integers, find the first missing positive integer in linear
 time and constant space. In other words, find the lowest positive integer that
 does not exist in the array. The array can contain duplicates and negative
@@ -9,10 +14,19 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should
 give 3.
 You can modify the input array in-place.
 -----------
+
+Solution 1:
 You can find some useful discussion for solving this problem from https://github.com/Jedshady/daily-coding-problem/blob/master/Problem%201-30/problem_4.py
 
 But here, you will find a optimized Complexity and Memory solution!
 RangedBinaryTree!
+
+
+Solution 2:
+Better solution but with more Memory space propsed by https://www.interviewbit.com hints. 
+In this solution each number must be on their right place, 
+negative number is not valid and will be set to zero
+
 '''
 
 class Node:
@@ -62,6 +76,18 @@ class Node:
         return '[%s~%s] (%s, %s)' % ( self.left_val, self.right_val, self.left, self.right)
 
 
+def firstMissingPositive(A):
+    n = len(A)
+    B = [0] * n
+    for i,x in enumerate(A):
+        if x>0 and x<=n: 
+            B[x-1] = x
+    for i,x in enumerate(B):
+        if x==0:
+            return i+1
+    return n+1
+
+
 if __name__ == '__main__':
 
     test_list = [[3, 4, -1, 1]]
@@ -70,5 +96,8 @@ if __name__ == '__main__':
         n = Node()
         for i in l:
             n.add(i)
-        print( 'input: ', l, ' output:', n.find_min_positive() )
+        
+        print( 'input: ', l, 
+               ' output by RagedBinaryTree:', n.find_min_positive(), 
+               'output by firstMissingPositive', firstMissingPositive(l) )
 
