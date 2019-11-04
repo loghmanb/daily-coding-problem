429a46084f60164d60cbec5fe1b0c042002d284f
Loghman Barari <loghmanb@gmail.com>
2019-11-04 20:20:59 +0330

    add link

--- oracle_set_matrix_zeros.py
+++ oracle_set_matrix_zeros.py
@@ -1,8 +1,9 @@
 '''
 Set Matrix Zeros
-Asked in:  
-Oracle
-Amazon
+Asked in: Oracle, Amazon
+
+https://www.interviewbit.com/problems/set-matrix-zeros/
+
 Given a matrix, A of size M x N of 0s and 1s. If an element is 0, set its entire row and column to 0.
 
 Note: This will be evaluated on the extra memory used. Try to minimize the space and time complexity.
