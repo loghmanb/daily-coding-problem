dddb21f21f1def3c8c4dbd0d1a3c4c8e38be447c
Loghman Barari <loghmanb@gmail.com>
2019-12-10 19:35:00 +0330

    solved by O(logN)

--- problem061_google_fast_pow.py
+++ problem061_google_fast_pow.py
@@ -8,6 +8,24 @@ Do this faster than the naive method of repeated multiplication.
 For example, pow(2, 10) should return 1024.
 '''
 
-def fast_pow(n, power):
-    pass
+def fast_pow(no, power):
+    ans = no
+    rem = 1
+    while power>1:
+        if power%2==1:
+            rem *= ans
+        power //= 2
+        if power>0:
+            ans *= ans
+    ans *= rem
+    return ans
 
+
+if __name__ == "__main__":
+    data = [
+            [[2, 10], 1024],
+            [[3, 13], 1594323]
+    ]
+
+    for d in data:
+        print('input', d[0], 'output', fast_pow(*d[0]), 'expected', d[1])
