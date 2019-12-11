58336cc3b9a7643ebe66cc39ed8b0698c13890ef
Loghman Barari <loghmanb@gmail.com>
2019-12-11 08:34:12 +0330

    add another example

--- problem060_facebook_multiset.py
+++ problem060_facebook_multiset.py
@@ -4,6 +4,8 @@ This problem was asked by Facebook.
 Given a multiset of integers, return whether it can be partitioned into two subsets whose sums are the same.
 
 For example, given the multiset {15, 5, 20, 10, 35, 15, 10}, it would return true, since we can split it up into {15, 5, 10, 15, 10} and {20, 35}, which both add up to 55.
+
+Given the multiset {15, 5, 20, 10, 35}, it would return false, since we can't split it up into two subsets that add up to the same sum.
 '''
 
 def isPartitionable(arr):
@@ -23,7 +25,8 @@ def isPartitionable(arr):
 
 if __name__ == "__main__":
     data = [
-            [[15, 5, 20, 10, 35, 15, 10], True]
+            [[15, 5, 20, 10, 35, 15, 10], True],
+            [[15, 5, 20, 10, 35], False]
     ]
     for d in data:
         print('input', d[0], 'output', isPartitionable(d[0]))
