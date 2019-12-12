a2d5539a4e65f6d1a83cf8266dc16e08312eac4f
loghmanb <loghmanb@gmail.com>
2019-12-12 08:36:03 +0330

    solved!

---
+++ problem074_apple_multiplication_table.py
@@ -0,0 +1,40 @@
+'''
+This problem was asked by Apple.
+
+Suppose you have a multiplication table that is N by N. That is, a 2D array where the value at the i-th row and j-th column is (i + 1) * (j + 1) (if 0-indexed) or i * j (if 1-indexed).
+
+Given integers N and X, write a function that returns the number of times X appears as a value in an N by N multiplication table.
+
+For example, given N = 6 and X = 12, you should return 4, since the multiplication table looks like this:
+
+| 1 | 2 | 3 | 4 | 5 | 6 |
+
+| 2 | 4 | 6 | 8 | 10 | 12 |
+
+| 3 | 6 | 9 | 12 | 15 | 18 |
+
+| 4 | 8 | 12 | 16 | 20 | 24 |
+
+| 5 | 10 | 15 | 20 | 25 | 30 |
+
+| 6 | 12 | 18 | 24 | 30 | 36 |
+
+And there are 4 12's in the table.
+'''
+
+def solve(N, No):
+    ans = 0
+    for i in range(2, min(N, No//2)+1):
+        if No%i==0:
+            ans += 1
+    if N>=No:
+        ans += 1
+    return ans
+
+
+if __name__ == "__main__":
+    data = [
+            [[6, 12], 4]
+    ]
+    for d in data:
+        print('input', d[0], 'output', solve(*d[0]))
