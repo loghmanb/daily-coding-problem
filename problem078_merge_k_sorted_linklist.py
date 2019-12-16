4f424f5d521ce94d9cbd4a383656aff9b9c961dd
loghmanb <loghmanb@gmail.com>
2019-12-16 13:09:46 +0330

    it has been already solved!

---
+++ problem078_merge_k_sorted_linklist.py
@@ -0,0 +1,9 @@
+'''
+This problem was asked by Google.
+
+Given k sorted singly linked lists, write a function to merge all the lists into one sorted singly linked list.
+
+It has already been solved in:
+
+https://github.com/loghmanb/daily-coding-problem/blob/master/google_merge_k_sorted_list.py
+'''
