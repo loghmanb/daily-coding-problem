53dc90a3f27ca2d36912d0c941e8bf53e9eb0028
loghmanb <loghmanb@gmail.com>
2019-12-16 22:16:06 +0330

    same question has been already solved!

---
+++ problem043_amazon_max_in_stack.py
@@ -0,0 +1,12 @@
+'''
+This problem was asked by Amazon.
+
+Implement a stack that has the following methods:
+
+push(val), which pushes an element onto the stack
+pop(), which pops off and returns the topmost element of the stack. If there are no elements in the stack, then it should throw an error or return null.
+max(), which returns the maximum value in the stack currently. If there are no elements in the stack, then it should throw an error or return null.
+Each method should run in constant time.
+'''
+
+# same question solved in https://github.com/loghmanb/daily-coding-problem/blob/master/amazon_min_stack.py
