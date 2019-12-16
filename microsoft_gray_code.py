19b36980409726b699903e60e4aa6f024125fc8b
loghmanb <loghmanb@gmail.com>
2019-12-16 16:44:07 +0330

    solved!

---
+++ microsoft_gray_code.py
@@ -0,0 +1,38 @@
+'''
+Gray Code
+Asked in: Microsoft
+
+https://www.interviewbit.com/problems/gray-code/
+
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+
+For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+There might be multiple gray code sequences possible for a given n.
+Return any such sequence.
+'''
+
+# @param N : integer
+# @return a list of integers
+def grayCode(n):
+    if n==1:
+        return [0, 1]
+    arr = grayCode(n-1)
+    cnt = 2**(n-1)
+    l = len(arr)
+    for i in range(l):
+        arr.append(cnt+arr[l-i-1])
+    return arr
+
+if __name__ == "__main__":
+    data = [
+            [5, [0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8, 24, 25, 27, 26, 30, 31, 29, 28, 20, 21, 23, 22, 18, 19, 17, 16]]
+    ]
+    for d in data:
+        print('input', d[0], 'output', grayCode(d[0]))
