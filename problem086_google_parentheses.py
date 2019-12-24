0cb76ebdbeb2912faf8eb75197e005871a2ff833
Loghman Barari <loghmanb@gmail.com>
2019-12-24 09:30:25 +0330

    solved by O(N)

---
+++ problem086_google_parentheses.py
@@ -0,0 +1,30 @@
+'''
+This problem was asked by Google.
+
+Given a string of parentheses, write a function to compute the minimum number of parentheses to be removed to make the string valid (i.e. each open parenthesis is eventually closed).
+
+For example, given the string "()())()", you should return 1. Given the string ")(", you should return 2, since we must remove all of them.
+'''
+
+def noOfWrongParentheses(string):
+    ans = 0
+    balance = 0
+    for i,ch in enumerate(string):
+        if ch=='(':
+            balance += 1
+        elif ch==')':
+            if balance>0:
+                balance -= 1
+            else:
+                ans += 1
+    ans += balance
+    return ans
+
+
+if __name__ == "__main__":
+    data = [
+            ["()())()", 1],
+            [")(", 2]
+    ]
+    for d in data:
+        print('input', d[0], 'output', noOfWrongParentheses(d[0]))
