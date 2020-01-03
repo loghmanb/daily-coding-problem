2e58e9e6291062961780715f0121453df9647d80
loghmanb <loghmanb@gmail.com>
2020-01-03 10:06:53 +0330

    solved!

--- facebook_stock_max_profit.py
+++ facebook_stock_max_profit.py
@@ -1,9 +1,12 @@
 '''
 This problem was asked by Facebook.
 
-Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
+Given a array of numbers representing the stock prices of a company in chronological order, 
+write a function that calculates the maximum profit you could have made from buying and selling that stock once. 
+You must buy before you can sell it.
 
-For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
+For example, given [9, 11, 8, 5, 7, 10], you should return 5, 
+since you could buy the stock at 5 dollars and sell it at 10 dollars.
 
 '''
 
@@ -24,4 +27,4 @@ if __name__ == "__main__":
             [[9, 11, 8, 5, 7, 10], 5]   
     ]
     for d in data:
-        print('input', d[0], maxProfit(d[0]))
+        print('input', d[0], 'output', maxProfit(d[0]))
