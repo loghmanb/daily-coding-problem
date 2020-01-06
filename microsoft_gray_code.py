7d6474c77762e3cb8da2d3047d74c7ae777b2cc9
Loghman Barari <loghmanb@gmail.com>
2020-01-06 11:09:48 +0330

    add non recursive solution!

--- microsoft_gray_code.py
+++ microsoft_gray_code.py
@@ -30,9 +30,22 @@ def grayCode(n):
         arr.append(cnt+arr[l-i-1])
     return arr
 
+def grayCodeLinear(n):
+    ans = []
+    if not n: return ans
+
+    ans = [0, 1]
+    for no in range(1, n):
+        l = len(ans)
+        const = 2**no
+        for j in range(l):
+            ans.append(const+ans[l-j-1])
+    return ans
+    
+
 if __name__ == "__main__":
     data = [
             [5, [0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8, 24, 25, 27, 26, 30, 31, 29, 28, 20, 21, 23, 22, 18, 19, 17, 16]]
     ]
     for d in data:
-        print('input', d[0], 'output', grayCode(d[0]))
+        print('input', d[0], 'output#1', grayCode(d[0]), 'output#2', grayCodeLinear(d[0]))
