b962bb714b16700e9ff1d97df56aebf834cc78ea
loghmanb <loghmanb@gmail.com>
2020-01-08 19:10:07 +0330

    review note!

--- walmart_seats.py
+++ walmart_seats.py
@@ -4,11 +4,13 @@ Asked in: Walmart labs
 
 https://www.interviewbit.com/problems/seats/
 
-There is a row of seats. Assume that it contains N seats adjacent to each other. There is a group of people who are already seated in that row randomly. i.e. some are sitting together & some are scattered.
+There is a row of seats. Assume that it contains N seats adjacent to each other. 
+There is a group of people who are already seated in that row randomly. i.e. some are sitting together & some are scattered.
 
 An occupied seat is marked with a character 'x' and an unoccupied seat is marked with a dot ('.')
 
-Now your target is to make the whole group sit together i.e. next to each other, without having any vacant seat between them in such a way that the total number of hops or jumps to move them should be minimum.
+Now your target is to make the whole group sit together i.e. next to each other, 
+without having any vacant seat between them in such a way that the total number of hops or jumps to move them should be minimum.
 
 Return minimum value % MOD where MOD = 10000003
 
