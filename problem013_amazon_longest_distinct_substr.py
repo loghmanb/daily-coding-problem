bcd906530edcc3b9c0b3334ea012a182d7005c98
loghmanb <loghmanb@gmail.com>
2020-01-08 08:09:18 +0330

    solved in optimized way!

--- problem013_amazon_longest_distinct_substr.py
+++ problem013_amazon_longest_distinct_substr.py
@@ -39,9 +39,33 @@ def longest_substr(s, k):
 
     return no
 
+def longest_substr_optimized(s, k):
+    letters = {}
+    start = 0
+    N = len(s)
+    max_len = 0
+    max_str = ''
+    for i,ch in enumerate(s):
+        if ch not in letters:
+            letters[ch] = 1
+        else:
+            letters[ch] += 1
+        l = len(letters.keys())
+        if l==k:
+            if max_len<(i-start+1):
+                max_str = s[start:i+1]
+                max_len = i-start+1
+        elif l>k:
+            while len(letters.keys())>k:
+                letters[s[start]] -= 1
+                if letters[s[start]]==0:
+                    del letters[s[start]]
+                start += 1
+    return max_str
+
 
 if __name__ == '__main__':
     test_list = [("abcba", 2)]
 
     for p in test_list:
-        print('input: ', p, '\toutput: ', longest_substr(*p))
+        print('input: ', p, '\toutput: ', longest_substr(*p), '\toutput optimized', longest_substr_optimized(*p))
