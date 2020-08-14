66209936be85c95debcf2f708cc8abbc9f1cb213
loghmanb <loghmanb@gmail.com>
2020-08-14 22:16:16 +0430

    [dp] used dp for having memory to solve it

--- problem075_microsoft_longest_increasing_subsequent.py
+++ problem075_microsoft_longest_increasing_subsequent.py
@@ -5,41 +5,62 @@ Given an array of numbers, find the length of the longest increasing subsequence
 
 For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
 '''
+import unittest
+
 import bisect
 
-def longest_increasing_subseq(arr, i=0, minVal=None):
-    if i>=len(arr)-1:
+
+def longest_inc_subseq_recursive(arr, minVal=None):
+    if len(arr) == 0:
         return 0
-    
-    l1 = 0
-    j = i
-    if minVal is not None:
-        while j<len(arr)-1 and arr[j]<minVal:
-            j += 1
-    if j<=len(arr)-1:
-        l1 = 1+longest_increasing_subseq(arr, j+1, arr[j])
-    l2 = longest_increasing_subseq(arr, i+1, minVal)
-    return max(l1, l2)
+    elif len(arr) == 1:
+        if minVal is None or arr[0] > minVal:
+            return 1
+        else:
+            return 0
+    else:
+        if minVal is None or arr[0] > minVal:
+            return max(1+longest_inc_subseq_recursive(arr[1:], arr[0]),
+                       longest_inc_subseq_recursive(arr[1:], minVal))
+        else:
+            return longest_inc_subseq_recursive(arr[1:], minVal)
 
 
-def longest_inc_subseq(arr):
+def longest_inc_subseq_dp(arr):
     longest_sub = []
     pre_len = 0
-    for i,x in enumerate(arr):
-        if i==0:
+    for i, x in enumerate(arr):
+        if i == 0:
             longest_sub.append((x, i, 1))
             pre_len = 1
         else:
             idx = bisect.bisect_left(longest_sub, (x, i, 0))
-            if idx>0: 
+            if idx > 0:
                 idx -= 1
                 pre_len = max(pre_len, longest_sub[idx][2]+1)
             longest_sub.insert(idx+1, (x, i, pre_len))
     return pre_len
 
+
+class SolutionTestCase(unittest.TestCase):
+
+    def test_recursive_method_1(self):
+        self.assertEqual(3, longest_inc_subseq_recursive(
+            [1, 2, 1, 5]))
+
+    def test_recursive_method_2(self):
+        self.assertEqual(6, longest_inc_subseq_recursive(
+            [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
+
+    def test_dp_method_1(self):
+        self.assertEqual(3, longest_inc_subseq_dp(
+            [1, 2, 1, 5]))
+
+    def test_dp_method_2(self):
+        self.assertEqual(6, longest_inc_subseq_dp(
+            [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
+
+
 if __name__ == "__main__":
-    data = [
-            [[0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], 6]
-    ]
-    for d in data:
-        print('input', d[0], 'output#1', longest_increasing_subseq(d[0]), 'output#2', longest_inc_subseq(d[0]))
+
+    unittest.main()
