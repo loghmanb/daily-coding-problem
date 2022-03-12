87d240e5ae5e7611e5bf739aa061f4aa266a7623
loghmanb <loghmanb@gmail.com>
2022-03-12 11:51:17 +0000

    implement in optimized way

--- booking_hotel_reviews.py
+++ booking_hotel_reviews.py
@@ -4,9 +4,13 @@ Asked in: Booking.com
 
 https://www.interviewbit.com/problems/hotel-reviews/
 
-Given a set of reviews provided by the customers for different hotels and a string containing “Good Words”, you need to sort the reviews in descending order according to their “Goodness Value” (Higher goodness value first). We define the “Goodness Value” of a string as the number of “Good Words” in that string.
+Given a set of reviews provided by the customers for different hotels and a string containing “Good Words”, 
+you need to sort the reviews in descending order according to their 
+“Goodness Value” (Higher goodness value first). We define the “Goodness Value” of a string 
+as the number of “Good Words” in that string.
 
-Note: Sorting should be stable. If review i and review j have the same “Goodness Value” then their original order would be preserved.
+Note: Sorting should be stable. If review i and review j have the same “Goodness Value” 
+then their original order would be preserved.
 
  You are expected to use Trie in an Interview for such problems 
 
@@ -27,7 +31,8 @@ Output:
 A vector V of integer which contain the original indexes of the reviews in the sorted order of reviews. 
 
 V[i] = k  means the review R[k] comes at i-th position in the sorted order. (See example below)
-In simple words, V[i]=Original index of the review which comes at i-th position in the sorted order. (Indexing is 0 based)
+In simple words, V[i]=Original index of the review which comes at i-th position in the sorted order. 
+(Indexing is 0 based)
 Example:
 
 Input: 
@@ -40,6 +45,16 @@ Here, sorted reviews are ["cool_wifi_speed", "water_is_cool", "cold_ice_drink"]
 '''
 
 import unittest
+
+
+def findHotelOrder2(S, R):
+    good_words = set(S.split('_'))
+    reviews = R
+    rank_idx_list = sorted(
+        [(sum(1 for s in review.split('_') if s in good_words), -idx) for idx, review in enumerate(reviews)], 
+        reverse=True)
+    return [-idx for _,idx in rank_idx_list]
+
 
 def findHotelOrder(S, R):
     S = set(S.split('_'))
@@ -61,6 +76,10 @@ class FindHotelOrderTest(unittest.TestCase):
         expected = [2, 0, 1]
         self.assertEqual(expected, result)
         
+    def test_findHotelOrder2_1(self):
+        result = findHotelOrder2("cool_ice_wifi", ["water_is_cool", "cold_ice_drink", "cool_wifi_speed"])
+        expected = [2, 0, 1]
+        self.assertEqual(expected, result)
 
 if __name__ == "__main__":
 
