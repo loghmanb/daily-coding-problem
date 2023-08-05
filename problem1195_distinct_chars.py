b2055dded4276611e0e7f1eb088e0027f603aa7b
Loghman Barari <loghmanb@gmail.com>
2023-08-05 18:12:38 +0000

    reformat

--- problem1195_distinct_chars.py
+++ problem1195_distinct_chars.py
@@ -1,5 +1,6 @@
 """
-Given a string, find the length of the smallest window that contains every distinct character. Characters may appear more than once in the window.
+Given a string, find the length of the smallest window that contains every distinct character. 
+Characters may appear more than once in the window.
 
 For example, given "jiujitsu", you should return 5, corresponding to the final five letters.
 """
@@ -10,13 +11,15 @@ def distinct_chars(s):
     d_chars = set()
     start = 0
     ans = 0
-    for i in range(len(s)):
+    for i, _ in enumerate(s):
         if s[i] not in d_chars:
             d_chars.add(s[i])
             ans = max(ans, i+1-start)
         else:
-            while s[start]==s[i]:
+            while s[start]!=s[i]:
+                d_chars.remove(s[start])
                 start += 1
+            start += 1
     return ans
     
 class TestDistinctCharsLen(unittest.TestCase):
--- problem201_google_triangl number.py
+++ problem201_google_triangl number.py
@@ -7,7 +7,8 @@ For example, [[1], [2, 3], [1, 5, 1]] represents the triangle:
   1
  2 3
 1 5 1
-We define a path in the triangle to start at the top and go down one row at a time to an adjacent value, eventually ending with an entry on the bottom row. 
+We define a path in the triangle to start at the top and go down one row at a time to an adjacent value, 
+eventually ending with an entry on the bottom row. 
 For example, 1 -> 3 -> 5. The weight of the path is the sum of the entries.
 
 Write a program that returns the weight of the maximum weight path.
